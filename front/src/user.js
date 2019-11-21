83461153c6070411d4ab3be25a742394a933269a
Adrien <adrien.demagny.com@gmail.com>
2019-11-21 18:54:14 +0100

    Front de l'ajout d'un user
    On peut ajouter un user en front

--- front/src/user.js
+++ front/src/user.js
@@ -16,4 +16,18 @@ export class User {
         const response = await axios.get('http://'+ config.host +'/user/getAll/');
         this.users = response.data.hits;
     }
+
+    async addUser() {
+      console.log("ADD " + this.name);
+      var data = {
+          name: this.name,
+          firstname: this.firstname,
+          adress: this.adress,
+          postalcode: this.postalcode,
+          city: this.city,
+          email: this.email
+      };
+      const response = await axios.post('http://'+ config.host + '/user/add/', data);
+  
+  }
 }
