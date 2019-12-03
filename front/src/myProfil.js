300050c07a82614913a96e1ccb75ccc8dcbd029b
Camille Moutte <mouttecam@gmail.com>
2019-12-03 19:07:14 +0100

    fix bug

--- front/src/myProfil.js
+++ front/src/myProfil.js
@@ -50,11 +50,11 @@ export class MyProfil {
     }
 
     goDetails(objectID) {
-        this.router.navigateToRoute('media', { objectID: objectID, isEdit: false});
+        this.router.navigateToRoute('media', { objectID: objectID, isEdit: true});
     }
 
     goDetailsUpdate(objectID) {
-        this.router.navigateToRoute('media', { objectID: objectID, isEdit: true});
+        this.router.navigateToRoute('media', { objectID: objectID, isEdit: false});
     }
 
     changed(objectID)
